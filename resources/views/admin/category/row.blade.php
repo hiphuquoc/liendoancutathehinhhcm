0df3825537ffc1088216d5be5ad66a8aee28f0f7
root <root@DESKTOP-3JVPJ2G.localdomain>
2025-02-06 18:20:24 +0700

    upcode for tranner update info self

--- resources/views/admin/category/row.blade.php
+++ resources/views/admin/category/row.blade.php
@@ -50,7 +50,7 @@
                     <div>Sửa</div>
                 </a>
             </div>
-            <div class="icon-wrapper iconAction">
+            {{-- <div class="icon-wrapper iconAction">
                 <a href="{{ route('admin.'.$typeRoute.'.view', ['id' => $item->id, 'language' => 'vi', 'type' => 'copy']) }}">
                     <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-copy"><rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect><path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path></svg>
                     <div>Chép</div>
@@ -65,17 +65,17 @@
                     </svg>
                     <div>Xóa</div>
                 </div>
-            </div>
+            </div> --}}
         </td>
     </tr>
-    <tr id="oneItemSub-{{ $item->id }}">
+    {{-- <tr id="oneItemSub-{{ $item->id }}">
         <td colspan="5"> 
             @include('admin.template.languageBox', [
                 'item' => $item,
                 'routeName' => 'admin.'.$typeRoute.'.view',
             ])
         </td>
-    </tr>
+    </tr> --}}
     <!-- nếu có phần tử con => viết tiếp -->
     @if(!empty($item->childs)&&$item->childs->isNotEmpty())
         @foreach($item->childs as $child)
